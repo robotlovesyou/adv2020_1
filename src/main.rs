684af1571abbeb48c5f20877deffb592a675ed50
Andrew Smith <andy@robotlovesyou.com>
2020-12-01 13:03:21 +0100

    apply clippy suggestions

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,12 @@ struct Error {
     message: String,
 }
 
+impl Error {
+    fn new(message: String) -> Error {
+        Error{message}
+    }
+}
+
 type Result<T> = result::Result<T, Error>;
 
 impl fmt::Display for Error {
@@ -24,13 +30,13 @@ impl error::Error for Error{}
 
 impl From<io::Error> for Error {
     fn from(e: io::Error) -> Self {
-        Error{message: format!("io error:{}", e)}
+        Error::new(format!("io error:{}", e))
     }
 }
 
 impl From<num::ParseIntError> for Error {
     fn from(e: ParseIntError) -> Self {
-        Error{message: format!("parse int error:{}", e)}
+        Error::new(format!("parse int error:{}", e))
     }
 }
 
@@ -39,7 +45,7 @@ fn read_lines<P: AsRef<Path>>(filename: P) -> io::Result<io::Lines<io::BufReader
     Ok(io::BufReader::new(file).lines())
 }
 
-fn read_ints(lines: impl Iterator<Item=io::Result<String>>) -> Result<Vec<i64>> {
+fn lines_to_ints(lines: impl Iterator<Item=io::Result<String>>) -> Result<Vec<i64>> {
     let mut result = Vec::new();
     for line in lines {
         result.push(line?.parse::<i64>()?);
@@ -48,26 +54,26 @@ fn read_ints(lines: impl Iterator<Item=io::Result<String>>) -> Result<Vec<i64>> 
 }
 
 fn find_result(ints: &mut Vec<i64>) -> Result<i64> {
-    ints.sort();
+    ints.sort_unstable();
     for i in ints.iter() {
         let rem = TARGET - i;
-        if let Ok(_) = ints.binary_search(&rem) {
+        if ints.binary_search(&rem).is_ok() {
             return Ok(i * rem);
         }
     }
-    Err(Error{message: "no result".to_string()})
+    Err(Error::new("no result".to_string()))
 }
 
 
 fn main() -> Result<()>{
     let args = env::args().collect::<Vec<String>>();
     if args.len() > 1 {
-        let mut ints = read_ints(read_lines(&args[1])?)?;
+        let mut ints = lines_to_ints(read_lines(&args[1])?)?;
         let result = find_result(&mut ints)?;
         println!("The result is {}", result);
         Ok(())
     } else {
-        Err(Error{message:"filename argument required".to_string()})
+        Err(Error::new("filename argument required".to_string()))
     }
 
 }
